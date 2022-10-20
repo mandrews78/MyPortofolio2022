c113bf6e74dadb7f09881c0b58ad1823560957f5
Michael Andrews <70594281+mandrews78@users.noreply.github.com>
2022-10-19 23:17:05 -0400

    updating portfolio

--- myportfolio/src/pages/GroupProjects.js
+++ myportfolio/src/pages/GroupProjects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProjectItem from '../components/ProjectItem'
 import EventFinder from '../images/EventFinderProject2.png'
 import RealEstate from '../images/realEstateProject.png'
+import PositiveThoughts from '../images/positivethoughts.png'
 import '../styles/Projects.css'
 
 function GroupProjects() {
@@ -11,6 +12,7 @@ function GroupProjects() {
       <div className='projectList'>
       <ProjectItem name="Event Finder" image={EventFinder} />
       <ProjectItem name='Top 5 Real Estate' image={RealEstate} />
+      <ProjectItem name="Positive Thoughts" image={PositiveThoughts} />
       </div>
     </div>
 
